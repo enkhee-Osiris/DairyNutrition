4b0f959fbae23b44b3628b45bfe28918443f9408
enkhee-Osiris <enkhee.ag@gmail.com>
2016-10-23 00:11:35 +0800

    Update

--- DairyNutrition/ViewController.swift
+++ DairyNutrition/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://httpbin.org/get").response { response in
+            print("Request: \(response.request)")
+            print("Response: \(response.response)")
+            print("Error: \(response.error)")
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
