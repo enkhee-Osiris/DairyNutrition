a867ec65777017b3a8ab83835c78deadac84eba6
enkhee-Osiris <enkhee.ag@gmail.com>
2016-10-22 23:27:57 +0800

    Initial Commit

---
+++ DairyNutrition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DairyNutrition
+//
+//  Created by Osiris on 2016-10-22.
+//  Copyright © 2016 Osiris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
